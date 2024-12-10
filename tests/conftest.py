bd4d3c50de126c12e8d637e716a3efe216506340
HetNagellakje <webdesign@t4ai.be>
2024-12-10 20:37:27 +0100

    Test procedure fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,4 @@
 """Test configuration for Project MasterMind."""
 
-import pytest
-
 # Register the asyncio plugin
 pytest_plugins = ['pytest_asyncio']
-
-def pytest_configure(config):
-    """Configure pytest for async testing."""
-    config.addini(
-        'asyncio_mode',
-        'default mode for async fixtures',
-        default='auto'
-    )
